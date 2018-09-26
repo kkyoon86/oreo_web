526b7b86a69cdac3ecd79bb0e0d54ac6f7b56dda
kkyoon86 <atunelf1@naver.com>
2018-09-26 18:00:24 +0900

    start

---
+++ js/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+
+
+
+
+
+});
+
+/*
+function setaddListeners() {
+  document.getElementsById('dxy').addEventListener('mousedown', mouseDown, false);
+  window.addEventListener('mouseup', mouseUp, false);
+
+}
+
+function mouseUp() {
+  window.removeEventListener('mousemove', divMove, true);
+}
+
+function mouseDown(e) {
+  window.addEventListener('mousemove', divMove, true);
+}
+
+function divMove(e) {
+  var div = document.getElementsById('dxy');
+  div.style.position = 'absolute';
+  div.style.top = e.clientY + 'px';
+  div.style.left = e.clientX + 'px';
+}
+*/
